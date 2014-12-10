c33eac192e1904ffe6831f63a65e378d42e81544
Krista Williams <krista@bluetrain.io>
2014-12-09 23:01:20 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name readtrackerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the readtrackerApp
+ */
+angular.module('readtrackerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name readtrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the readtrackerApp
+ */
+angular.module('readtrackerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
